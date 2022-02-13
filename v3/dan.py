7c3da3fcf6e1ad7ce76df7e85b6298bf166bafb5
stuartrucker <stuart.a.rucker@gmail.com>
2022-02-13 14:10:27 -0500

    dan

---
+++ v3/dan.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+
+class DAN(nn.Module):
+    def __init__(self, num_embeddings, embed_dimension=1000, intermediate_dimension=1000, no_word_token=0):
+        super(DAN, self).__init__()
+        
+        self.embed = nn.EmbeddingBag(num_embeddings, embed_dimension, sparse=True, mode='sum')
+        self.fc1 = nn.Linear(embed_dimension, embed_dimension)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(embed_dimension, 2)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        embedded = self.embed(x)/x.size(dim=1)
+        inter1 = self.relu(self.fc1(embedded))
+        return self.softmax(self.fc2(inter1))
